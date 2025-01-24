b6ca2295dcddbcb0e09ecd4aec4456426cc00805
João Layon <104402531+DuJao22@users.noreply.github.com>
2025-01-23 23:49:07 -0300

    Delete Dados.py

--- Dados.py
+++
@@ -1,2 +0,0 @@
-Usuarios_permitidos = {"dujao":[1234,"Joao layon","Linha3"]}
-Pecas = {"linha1":{},"linha2":{},"Linha3":{"Pulse":{"cinto_dx":"01002661890031110852003523",},"Argo":{"pedaleira_550":"00521918550011060506603223"}}}
